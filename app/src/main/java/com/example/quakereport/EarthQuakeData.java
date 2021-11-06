413f2d082ed09bb6181e796e332a80ec4ef44684
Abizer-R <rabizeridris@gmail.com>
2021-11-06 21:24:42 +0530

    Added networking

--- app/src/main/java/com/example/quakereport/EarthQuakeData.java
+++ app/src/main/java/com/example/quakereport/EarthQuakeData.java
@@ -4,11 +4,13 @@ public class EarthQuakeData {
     private Double magnitude;
     private String place;
     private long timeInMilliseconds;
+    private String url;
 
-    public EarthQuakeData(Double magnitude, String place, long timeInMilliseconds) {
+    public EarthQuakeData(Double magnitude, String place, long timeInMilliseconds, String url) {
         this.magnitude = magnitude;
         this.place = place;
         this.timeInMilliseconds = timeInMilliseconds;
+        this.url = url;
     }
 
     public Double getMagnitude() {
@@ -33,5 +35,13 @@ public class EarthQuakeData {
 
     public void setTimeInMilliseconds(long timeInMilliseconds) {
         this.timeInMilliseconds = timeInMilliseconds;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
--- app/src/main/java/com/example/quakereport/MainActivity.java
+++ app/src/main/java/com/example/quakereport/MainActivity.java
@@ -4,38 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EdgeEffect;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String websiteUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+
+//    min magnitude 6: https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<EarthQuakeData> earthquakes = QueryUtils.extractEarthquakes();
-
-//        ArrayList<EarthQuakeData> earthquakes = new ArrayList<>();
+//        ArrayList<EarthQuakeData> earthquakes = QueryUtils.extractEarthquakes();
 //
-//        earthquakes.add(new EarthQuakeData("1.0", "Kalyan","1-2-3"));
-//        earthquakes.add(new EarthQuakeData("2.0", "Mumbai", "1-2-3"));
-//        earthquakes.add(new EarthQuakeData("3.0", "Indore", "1-2-3"));
-//        earthquakes.add(new EarthQuakeData("4.0", "Rampura", "1-2-3"));
-//        earthquakes.add(new EarthQuakeData("5.0", "Neemuch", "1-2-3"));
-//        earthquakes.add(new EarthQuakeData("6.0", "Kurla", "1-2-3"));
-//        earthquakes.add(new EarthQuakeData("7.0", "Hyderabad", "1-2-3"));
+//
+//        ListView earthQuakeListView = findViewById(R.id.list_view);
+//
+//        EarthQuakeAdapter earthQuakeAdapter = new EarthQuakeAdapter(this, 0, earthquakes);
+//
+//        earthQuakeListView.setAdapter(earthQuakeAdapter);
+//
+//        earthQuakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                // TODO Set item Click Listener
+//            }
+//        });
 
+        EarthquakeTask task = new EarthquakeTask();
+
+        task.execute(websiteUrl);
+
+    }
+
+    private class EarthquakeTask extends AsyncTask<String, Void, List<EarthQuakeData>> {
+
+        @Override
+        protected List<EarthQuakeData> doInBackground(String... strings) {
+            if(strings.length < 1 || strings[0] == null)
+                return null;
+            List<EarthQuakeData> earthquakes = QueryUtils.fetchEarthquakeData(strings[0]);
+            return earthquakes;
+        }
+
+        @Override
+        protected void onPostExecute(List<EarthQuakeData> earthquakes) {
+            if(earthquakes.size() < 1)
+                return;
+            updateUi(earthquakes);
+        }
+    }
+
+    public void updateUi(List<EarthQuakeData> earthquakes) {
         ListView earthQuakeListView = findViewById(R.id.list_view);
 
-        EarthQuakeAdapter earthQuakeAdapter = new EarthQuakeAdapter(this, 0, earthquakes);
+        EarthQuakeAdapter earthQuakeAdapter = new EarthQuakeAdapter(MainActivity.this, 0, earthquakes);
 
         earthQuakeListView.setAdapter(earthQuakeAdapter);
 
@@ -43,9 +75,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Intent launchWebsite = new Intent(Intent.ACTION_VIEW);
-                launchWebsite.setData(Uri.parse(QueryUtils.websiteUrl()));
-                startActivity(launchWebsite);
+                EarthQuakeData currEarthquake = earthQuakeAdapter.getItem(i);
+
+                Uri earthquakeUri = Uri.parse(currEarthquake.getUrl());
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                startActivity(browserIntent);
             }
         });
     }
